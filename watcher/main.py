183b0bc46ebe56b2476add1f2224dcf6dd3fde5a
Isaac <isaacnoboa@gmail.com>
2024-02-11 09:05:49 -0400

    alright fr this time

--- watcher/main.py
+++ watcher/main.py
@@ -5,3 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def home_view():
     return render_template('index.html')
+
+@app.route("/webhooks/<webhook_id>/<webhook_token>")
+def webhooks_view():
+    return render_template('webhooks.html')
